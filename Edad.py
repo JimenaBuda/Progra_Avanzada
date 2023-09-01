92829345df9100063a3761beb15168cf514a453c
Jimena Buda <143077053+JimenaBuda@users.noreply.github.com>
2023-09-01 08:00:00 -0600

    Add files via upload

---
+++ Edad.py
@@ -0,0 +1,9 @@
+#mayor o menor de edad
+
+edad=int(input("Dame tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad")
+
+else:
+    print("Eres menor de edad")
---
+++ Suma.py
@@ -0,0 +1,6 @@
+#suma de dos numeros
+num1=int(input("Escribe un número: "))
+num2=int(input("Escribe otro número: "))
+total=num1+num2
+
+print("El resultado es: ",total)
